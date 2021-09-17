eeb498d5862c9e9245bdc87c7a105cb98e963877
Moses-Yu <mosesyumss@gmail.com>
2021-09-17 17:56:54 +0900

    ReMerge

--- src/git_test2/Main.java
+++ src/git_test2/Main.java
@@ -3,15 +3,8 @@ package git_test2;
 public class Main {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
-        Hello hello = new Hello();
+		Hello hello = new Hello("hello world");
         System.out.println(hello.say());
-=======
-
-        Hello hello = new Hello("hello world");
-        hello.say();
-
->>>>>>> 3d2786bb988cd078dccc8f6b4c57bff7aad3d9c9
     }
 
 }
